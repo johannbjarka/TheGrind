03fc9a982ce2bd72457104ec9bca2e73a0f1e1b2
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-05-04 15:52:58 +0000

    Saved scene

---
+++ Assets/Scripts/ComputerMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComputerMenu : MonoBehaviour {
+
+	public Canvas ComputerMenuCanvas;
+	bool menuIsOpen = false;
+
+	public void openComputerMenuPanel () {
+		menuIsOpen = !menuIsOpen;
+		ComputerMenuCanvas.enabled = !ComputerMenuCanvas.enabled;
+	}
+
+
+}

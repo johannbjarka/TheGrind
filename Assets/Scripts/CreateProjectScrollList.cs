51a8d1797d30a486e07aa0db348a72d0d5d51341
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-05-05 19:57:33 +0000

    Available projects list complete

---
+++ Assets/Scripts/CreateProjectScrollList.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ProjectItem {
+	public string ProjectName;
+	public string ProjectReward;
+	public string ProjectDescription;
+	public string ProjectPenalty;
+	public string ProjectDeadline;
+	public string ProjectCategory;
+}
+
+public class CreateProjectScrollList : MonoBehaviour {
+	public GameObject projectPanel;
+	public List<ProjectItem> projectList;
+
+	public Transform ProjectContentPanel;
+
+	void Start () {
+		PopulateProjectList ();
+	}
+
+	void PopulateProjectList () {
+		foreach (var project in projectList) {
+			GameObject newProjectPanel = Instantiate (projectPanel) as GameObject;
+			ProjectPanel panel = newProjectPanel.GetComponent <ProjectPanel> ();
+			panel.Name.text = project.ProjectName;
+			panel.Deadline.text = project.ProjectDeadline;
+			panel.Description.text = project.ProjectDescription;
+			panel.Reward.text = project.ProjectReward;
+			panel.Penalty.text = project.ProjectPenalty;
+			panel.Category.text = project.ProjectCategory;
+			newProjectPanel.transform.SetParent (ProjectContentPanel);
+			panel.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/ProjectPanel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProjectPanel : MonoBehaviour {
+	public GameObject ProjPanel;
+	public Text Name;
+	public Text Description;
+	public Text Reward;
+	public Text Penalty;
+	public Text Deadline;
+	public Text Category;
+	public Button AcceptButton;
+}

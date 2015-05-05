755fc43c741970e4b0e4b07830159a315bcdc4f2
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-05-05 19:57:47 +0000

    Script for available projects

--- Assets/Scripts/ComputerMenu.cs
+++ Assets/Scripts/ComputerMenu.cs
@@ -4,10 +4,25 @@ using System.Collections;
 public class ComputerMenu : MonoBehaviour {
 
 	public Canvas ComputerMenuCanvas;
+	public Canvas EmployeeRecordsCanvas;
+	public Canvas AvailableProjectsCanvas;
+
 	bool menuIsOpen = false;
+	bool employeeRecordsIsOpen = false;
+	bool availableProjectsIsOpen = false;
 
 	public void openComputerMenuPanel () {
 		menuIsOpen = !menuIsOpen;
 		ComputerMenuCanvas.enabled = !ComputerMenuCanvas.enabled;
+	}
+
+	public void openEmployeeRecords () {
+		employeeRecordsIsOpen = !employeeRecordsIsOpen;
+		EmployeeRecordsCanvas.enabled = !EmployeeRecordsCanvas.enabled;
+	}
+
+	public void openAvailableProjects () {
+		availableProjectsIsOpen = !availableProjectsIsOpen;
+		AvailableProjectsCanvas.enabled = !AvailableProjectsCanvas.enabled;
 	}
 }

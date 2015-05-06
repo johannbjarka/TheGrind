7c30dbe85e1d51f63946e34ccbe687d76d2e88bf
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-05-06 16:02:15 +0000

    prefab change

--- Assets/Scripts/CreateScrollList.cs
+++ Assets/Scripts/CreateScrollList.cs
@@ -56,4 +56,5 @@ public class CreateScrollList : MonoBehaviour {
 			GameObject.Destroy(child.gameObject);
 		}
 	}
+
 }
--- Assets/Scripts/EmployeePanel.cs
+++ Assets/Scripts/EmployeePanel.cs
@@ -3,13 +3,14 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class EmployeePanel : MonoBehaviour {
-		public GameObject panel;
-		public Image employeeIcon;
-		public Text speedLabel;
-		public Text nameLabel;
-		public Text genderLabel;
-		public Text salaryLabel;
-		public Text moraleLabel;
-		public Button skillsButton;
-		public Button hireButton;
+	public GameObject panel;
+	public Image employeeIcon;
+	public Text speedLabel;
+	public Text nameLabel;
+	public Text genderLabel;
+	public Text salaryLabel;
+	public Text moraleLabel;
+	public Button skillsButton;
+	public Button hireButton;
+	public Button fireButton;
 }

44cf6a25e62aaa859abc118bc1d9bc3d3bb30863
Jóhann Örn Bjarkason <johannbjarka@gmail.com>
2015-05-10 20:27:43 +0000

    Started on cancel project

---
+++ Assets/Scripts/CancelProject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CancelProject : MonoBehaviour {
+
+	public void cancel(int id) {
+		Company myCompany = GameObject.Find("Company").GetComponent<Company>();
+		foreach(var proj in myCompany.projects) {
+			if(id == proj.ID) {
+				foreach(Character emp in proj.employees) {
+					emp.onProject = false;
+				}
+				proj.employees.Clear();
+				myCompany.availableProjects.Add(proj);
+				myCompany.projects.Remove(proj);
+				break;
+			}
+		}
+	}
+}

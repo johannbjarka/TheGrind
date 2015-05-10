d0bd538bc14ca438246c6d7a8e01f2c7b91802f1
ingolfurh <ingolfurhalld@gmail.com>
2015-05-10 18:27:07 +0000

    Komið rudimentary water drinking path finding

--- Assets/Scripts/CharacterAI.cs
+++ Assets/Scripts/CharacterAI.cs
@@ -14,12 +14,12 @@ public class CharacterAI : MonoBehaviour {
 
 	void Start(){
 		watercoolers = new Vector2[3];
-		watercoolers[0].x = 0.01f;
-		watercoolers[0].y = 4.19f;
-		watercoolers[1].x = -8.66f;
-		watercoolers[1].y = 0.679f;
-		watercoolers[2].x = 8.656f;
-		watercoolers[2].y = 0.682f;
+		watercoolers[0].x = -8.66f;
+		watercoolers[0].y = 0.879f;
+		watercoolers[1].x = 8.656f;
+		watercoolers[1].y = 0.882f;
+		watercoolers[2].x = 0.01f;
+		watercoolers[2].y = 4.19f;
 	}
 
 	void Update(){
@@ -49,43 +49,87 @@ public class CharacterAI : MonoBehaviour {
 		//Get closest watercooler
 		Vector2 myPosition = new Vector2(x, y);
 		int closest = 0;
-		for(int i = 1; i < 3; i++){
+		for(int i = 1; i < 2; i++){
 			if(Vector2.Distance(watercoolers[i], myPosition) < Vector2.Distance(watercoolers[closest], myPosition)){
 				closest = i;
 			}
 		}
 
-		//Travel to watercoolers[i]
-		if(watercoolers[closest].x > x + 0.3) {
-			//Go to the right until x == watercoolers[closest].x
-			bool mov1 = true;
+		if((-8.34f < x && x < -5.71f) 
+		   || (-4.221f < x && x < -1.421f)
+		   || (-0.725f < x && x < 0.745f)
+		   || (1.43f < x && x < 4.23f)
+		   || (5.667f < x && x < 8.34f)) {
 
-			me.anim.SetInteger ("Direction", 3);
-			me.anim.SetBool ("Moving", mov1);
-			me.anim.IsInTransition (0);
-			me.anim.speed = 5;
-			if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_East")) {
-				transform.position += Vector3.right * me.movSpeed;
+			//Travel to watercoolers[i]
+			if(watercoolers[closest].x > x + 0.6) {
+				//Go to the right until x == watercoolers[closest].x
+				bool mov1 = true;
+
+				me.anim.SetInteger ("Direction", 3);
+				me.anim.SetBool ("Moving", mov1);
+				me.anim.IsInTransition (0);
+				me.anim.speed = 5;
+				if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_East")) {
+					transform.position += Vector3.right * me.movSpeed;
+				}
+				mov1 = false;
+				//me.anim.speed = 1;
 			}
-			mov1 = false;
-			//me.anim.speed = 1;
-		}
-		else if(watercoolers[closest].x < x - 0.3) {
-			//Go to the left until x == watercoolers[closest].x
-			bool mov1 = true;
-			
-			me.anim.SetInteger ("Direction", 1);
-			me.anim.SetBool ("Moving", mov1);
-			me.anim.IsInTransition (0);
-			me.anim.speed = 5;
-			if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_West")) {
-				transform.position += Vector3.left * me.movSpeed;
+			else if(watercoolers[closest].x < x - 0.6) {
+				//Go to the left until x == watercoolers[closest].x
+				bool mov1 = true;
+				
+				me.anim.SetInteger ("Direction", 1);
+				me.anim.SetBool ("Moving", mov1);
+				me.anim.IsInTransition (0);
+				me.anim.speed = 5;
+				if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_West")) {
+					transform.position += Vector3.left * me.movSpeed;
+				}
+				mov1 = false;
+				//me.anim.speed = 1;
 			}
-			mov1 = false;
-			//me.anim.speed = 1;
+			else {
+				if(watercoolers[closest].y > y + 0.8) {
+					//Go north until x == watercoolers[closest].x
+					bool mov1 = true;
+					
+					me.anim.SetInteger ("Direction", 2);
+					me.anim.SetBool ("Moving", mov1);
+					me.anim.IsInTransition (0);
+					me.anim.speed = 5;
+					if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_North")) {
+						transform.position += Vector3.up * me.movSpeed;
+					}
+					mov1 = false;
+					//me.anim.speed = 1;
+				}
+				else if(watercoolers[closest].y < y - 0.3) {
+					//Go south until x == watercoolers[closest].x
+					bool mov1 = true;
+					
+					me.anim.SetInteger ("Direction", 0);
+					me.anim.SetBool ("Moving", mov1);
+					me.anim.IsInTransition (0);
+					me.anim.speed = 5;
+					if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_South")) {
+						transform.position += Vector3.down * me.movSpeed;
+					}
+					mov1 = false;
+					//me.anim.speed = 1;
+				}
+				else {
+					//Do nothing
+					//Play drinking animation?
+					thirsty = 0;
+					me.anim.speed = 0.7f;
+				}
+			}
 		}
 		else {
-			if(watercoolers[closest].y > y + 1.0) {
+			//Travel to watercoolers[i]
+			if(watercoolers[closest].y > y + 0.8) {
 				//Go north until x == watercoolers[closest].x
 				bool mov1 = true;
 				
@@ -114,13 +158,42 @@ public class CharacterAI : MonoBehaviour {
 				//me.anim.speed = 1;
 			}
 			else {
-				//Do nothing
-				//Play drinking animation?
-				thirsty = 0;
-				me.anim.speed = 0.7f;
+				if(watercoolers[closest].x > x + 0.6) {
+					//Go to the right until x == watercoolers[closest].x
+					bool mov1 = true;
+					
+					me.anim.SetInteger ("Direction", 3);
+					me.anim.SetBool ("Moving", mov1);
+					me.anim.IsInTransition (0);
+					me.anim.speed = 5;
+					if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_East")) {
+						transform.position += Vector3.right * me.movSpeed;
+					}
+					mov1 = false;
+					//me.anim.speed = 1;
+				}
+				else if(watercoolers[closest].x < x - 0.6) {
+					//Go to the left until x == watercoolers[closest].x
+					bool mov1 = true;
+					
+					me.anim.SetInteger ("Direction", 1);
+					me.anim.SetBool ("Moving", mov1);
+					me.anim.IsInTransition (0);
+					me.anim.speed = 5;
+					if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_West")) {
+						transform.position += Vector3.left * me.movSpeed;
+					}
+					mov1 = false;
+					//me.anim.speed = 1;
+				}
+				else {
+					//Do nothing
+					//Play drinking animation?
+					thirsty = 0;
+					me.anim.speed = 0.7f;
+				}
 			}
 		}
-
 		/*
 		//me.transform.position(watercoolers[closest].x, myPosition.y);
 		//gameObject.transform.position = new Vector3(watercoolers[closest].x, watercoolers[closest].y, 0);
---
+++ Assets/Scripts/WalkAroundObject.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkAroundObject : MonoBehaviour {
+
+	public Character me;
+
+	void Awake () {
+		me = gameObject.GetComponent<Character>();
+	}
+
+	void OnCollisionEnter2d(Collision col) {
+		Debug.Log("Collision");
+		if(col.gameObject.transform.position.x > gameObject.transform.position.x) {
+			if(col.gameObject.transform.position.y > gameObject.transform.position.y) {
+				//Go south
+				bool mov1 = true;
+				
+				me.anim.SetInteger ("Direction", 0);
+				me.anim.SetBool ("Moving", mov1);
+				me.anim.IsInTransition (0);
+				me.anim.speed = 5;
+				if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_South")) {
+					transform.position += Vector3.down * me.movSpeed;
+				}
+				mov1 = false;
+			}
+			else {
+				//Go north
+				bool mov1 = true;
+				
+				me.anim.SetInteger ("Direction", 2);
+				me.anim.SetBool ("Moving", mov1);
+				me.anim.IsInTransition (0);
+				me.anim.speed = 5;
+				if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_North")) {
+					transform.position += Vector3.up * me.movSpeed;
+				}
+				mov1 = false;
+			}
+		}
+		else {
+			if(col.gameObject.transform.position.y > gameObject.transform.position.y) {
+				//Go south
+				bool mov1 = true;
+				
+				me.anim.SetInteger ("Direction", 0);
+				me.anim.SetBool ("Moving", mov1);
+				me.anim.IsInTransition (0);
+				me.anim.speed = 5;
+				if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_South")) {
+					transform.position += Vector3.down * me.movSpeed;
+				}
+				mov1 = false;
+			}
+			else {
+				//Go north
+				bool mov1 = true;
+				
+				me.anim.SetInteger ("Direction", 2);
+				me.anim.SetBool ("Moving", mov1);
+				me.anim.IsInTransition (0);
+				me.anim.speed = 5;
+				if(me.anim.GetCurrentAnimatorStateInfo(0).IsName("Char_Walk_North")) {
+					transform.position += Vector3.up * me.movSpeed;
+				}
+				mov1 = false;
+			}
+		}
+	}
+}

cce49937c46d5c96cafd66481c04c54644c7df4d
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-05-12 22:31:41 +0000

    Started work on transitions.

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Prime31.TransitionKit;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void fadeToContext () {
+		var fader = new FadeTransition () {
+			nextScene = Application.loadedLevel == 0 ? 1 : 0,
+			fadedDelay = 0.1f,
+			fadeToColor = Color.black
+		};
+		TransitionKit.instance.transitionWithDelegate(fader);
+	}
+
+	public void fadeToTutorial () {
+		/*
+		var pixelater = new PixelateTransition() {
+			nextScene = Application.loadedLevel == 1 ? 2 : 1,
+			finalScaleEffect = PixelateTransition.PixelateFinalScaleEffect.ToPoint,
+			duration = 1.0f
+		};
+		TransitionKit.instance.transitionWithDelegate(pixelater);
+		*/
+
+		var winder = new WindTransition () {
+			nextScene = 2,
+			duration = 1.0f
+		};
+		TransitionKit.instance.transitionWithDelegate(winder);
+	}
+
+	public void fadeToOffice () {
+		var winder = new WindTransition () {
+			nextScene = 3,
+			duration = 1.0f
+		};
+		TransitionKit.instance.transitionWithDelegate(winder);
+		/*
+		var fader = new FadeTransition () {
+			nextScene = Application.loadedLevel == 2 ? 3 : 2,
+			fadedDelay = 0.1f,
+			fadeToColor = Color.black
+		};
+		TransitionKit.instance.transitionWithDelegate(fader);
+		*/
+	}
+
+	public void fadeToNextWeek () {
+		// Problem with this is the transition happens after all changes, so the illusion of a transition is lost.
+		var fader = new FadeTransition () {
+			fadedDelay = 0.01f,
+			fadeToColor = Color.black
+		};
+		TransitionKit.instance.transitionWithDelegate(fader);
+	}
+}

1bbb5de497d5f5f5153771f9ed9c5f51e9106755
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-05-13 13:58:25 +0000

    Tutoral WIP

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+
+	public GameObject welcomePanel;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void closeWelcome() {
+		welcomePanel.SetActive (false);
+	}
+}

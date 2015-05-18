b241f6a1d6d8906d5110d3e3068b5c36743e2cf8
Jóhann Örn Bjarkason <johannbjarka@gmail.com>
2015-05-18 15:24:01 +0000

    Changed yell sound, added hello sounds when employees are clicked

---
+++ Assets/Scripts/HelloGirl.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloGirl : MonoBehaviour {
+
+	public void playSound () {
+		GetComponent<AudioSource>().Play();
+	}
+}
---
+++ Assets/Scripts/HelloMan.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloMan : MonoBehaviour {
+
+	public void playSound () {
+		GetComponent<AudioSource>().Play();
+	}
+}
